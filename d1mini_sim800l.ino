c5957db21d934a8133dc8cbdf94c13f446707a34
skemppinen <82161700+skemppinen@users.noreply.github.com>
2025-01-05 17:51:41 +0200

    Update d1mini_sim800l.ino

--- d1mini_sim800l.ino
+++ d1mini_sim800l.ino
@@ -1,29 +1,3 @@
-/**
-LoLin d1Mini Lite + SIM800L V2
-use external powersource, 5V + 2A
-
-Connect SIM800l VCC to external powersource +5V 
-Connect SIM800l GND to external powersource GND
-Connect d1Mini D1 to SIM800l RXD
-Connect d1Mini D2 to SIM800l TXD
-Connect d1Mini GND to Sim800l V2 TTL GND
-*/
-/**
-Register SIM800 to network 
-AT+CREG=1
-OK
-AT+SAPBR=3,1,"CONTYPE","GPRS"
-OK
-AT+SAPBR=3,1,"APN","prepaid.dna.fi" //replace APN address based on your ISP
-OK
-AT+SAPBR=1,1
-OK
-AT+CREG?
-+CREG: 1,1 // 1,1 means that SIM800l is connected into your "home network".
-
-SIM800l led blinks two times per second = Connected
-*/
-
 #include <SoftwareSerial.h>
 SoftwareSerial sim800l(D1, D2); // RX, TX
 
